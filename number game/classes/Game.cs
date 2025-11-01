a846e41dc2de4155ca94f11cfb4098edc020f7b0
lasha <maisuradze.llasha@gmail.com>
2025-11-01 22:12:40 +0400

    little fix

--- number game/classes/Game.cs
+++ number game/classes/Game.cs
@@ -14,131 +14,139 @@ namespace number_game.classes
             Console.WriteLine("1. Press E for Easy (1-10)" +
                 "\n2. Press M for Medium (1-20)" +
                 "\n3. Press H Hard (1-50)");
-            char difficulty = Char.ToUpper(Console.ReadKey().KeyChar);
-            Console.Clear();
-            if (difficulty == 'E')
+            while (true)
             {
-                Random rand = new Random();
-                int numberToGuess = rand.Next(1, 11);
-                Console.WriteLine("You have chosen Easy difficulty." +
-                    "Guess a number between 1 and 10.");
-                int userGuess = Int32.Parse(Console.ReadLine());
+                char difficulty = Char.ToUpper(Console.ReadKey().KeyChar);
                 Console.Clear();
-                while (true)
+                if(difficulty != 'E' && difficulty != 'M' && difficulty != 'H')
                 {
-                    if(userGuess <= 0 || userGuess > 10)
+                    Console.WriteLine("Invalid input please choose E, M or H");
+                    continue;
+                }
+                else if (difficulty == 'E')
+                {
+                    Random rand = new Random();
+                    int numberToGuess = rand.Next(1, 11);
+                    Console.WriteLine("You have chosen Easy difficulty." +
+                        "Guess a number between 1 and 10.");
+                    int userGuess = Int32.Parse(Console.ReadLine());
+                    Console.Clear();
+                    while (true)
                     {
-                        Console.WriteLine("please enter a number from 1 to 10");
-                        userGuess = Int32.Parse(Console.ReadLine());
-                        Console.Clear();
-                    }
-                    else if (userGuess < numberToGuess)
-                    {
-                        Console.WriteLine("try higher");
-                        userGuess = Int32.Parse(Console.ReadLine());
-                        Console.Clear();
-                    }
-                    else if (userGuess > numberToGuess)
-                    {
-                        Console.WriteLine("try lower");
-                        userGuess = Int32.Parse(Console.ReadLine());
-                        Console.Clear();
-                    }
-                    else if (userGuess == numberToGuess)
-                    {
-                        Console.WriteLine("Congrats You guessed the right number");
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input");
-                        userGuess = Int32.Parse(Console.ReadLine());
+                        if (userGuess <= 0 || userGuess > 10)
+                        {
+                            Console.WriteLine("please enter a number from 1 to 10");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                            Console.Clear();
+                        }
+                        else if (userGuess < numberToGuess)
+                        {
+                            Console.WriteLine("try higher");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                            Console.Clear();
+                        }
+                        else if (userGuess > numberToGuess)
+                        {
+                            Console.WriteLine("try lower");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                            Console.Clear();
+                        }
+                        else if (userGuess == numberToGuess)
+                        {
+                            Console.WriteLine("Congrats You guessed the right number");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                        }
                     }
                 }
-            }
-            else if (difficulty == 'M')
-            {
-                Random rand = new Random();
-                int numberToGuess = rand.Next(1, 21);
-                Console.WriteLine("You have chosen Medium difficulty." +
-                    "Guess a number between 1 and 20.");
-                int userGuess = Int32.Parse(Console.ReadLine());
-                Console.Clear();
-                while (true)
+                else if (difficulty == 'M')
                 {
-                    if (userGuess <= 0 || userGuess > 20)
+                    Random rand = new Random();
+                    int numberToGuess = rand.Next(1, 21);
+                    Console.WriteLine("You have chosen Medium difficulty." +
+                        "Guess a number between 1 and 20.");
+                    int userGuess = Int32.Parse(Console.ReadLine());
+                    Console.Clear();
+                    while (true)
                     {
-                        Console.WriteLine("please enter a number from 1 to 20");
-                        userGuess = Int32.Parse(Console.ReadLine());
-                        Console.Clear();
-                    }
-                    else if (userGuess < numberToGuess)
-                    {
-                        Console.WriteLine("try higher");
-                        userGuess = Int32.Parse(Console.ReadLine());
-                        Console.Clear();
-                    }
-                    else if (userGuess > numberToGuess)
-                    {
-                        Console.WriteLine("try lower");
-                        userGuess = Int32.Parse(Console.ReadLine());
-                        Console.Clear();
-                    }
-                    else if (userGuess == numberToGuess)
-                    {
-                        Console.WriteLine("Congrats You guessed the right number");
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input");
-                        userGuess = Int32.Parse(Console.ReadLine());
+                        if (userGuess <= 0 || userGuess > 20)
+                        {
+                            Console.WriteLine("please enter a number from 1 to 20");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                            Console.Clear();
+                        }
+                        else if (userGuess < numberToGuess)
+                        {
+                            Console.WriteLine("try higher");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                            Console.Clear();
+                        }
+                        else if (userGuess > numberToGuess)
+                        {
+                            Console.WriteLine("try lower");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                            Console.Clear();
+                        }
+                        else if (userGuess == numberToGuess)
+                        {
+                            Console.WriteLine("Congrats You guessed the right number");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                        }
                     }
                 }
-            }
-            else if (difficulty == 'H')
-            {
-                Random rand = new Random();
-                int numberToGuess = rand.Next(1, 51);
-                Console.WriteLine("You have chosen Hard difficulty." +
-                    "Guess a number between 1 and 50.");
-                int userGuess = Int32.Parse(Console.ReadLine());
-                Console.Clear();
-                while (true)
+                else if (difficulty == 'H')
                 {
-                    if (userGuess <= 0 || userGuess > 50)
+                    Random rand = new Random();
+                    int numberToGuess = rand.Next(1, 51);
+                    Console.WriteLine("You have chosen Hard difficulty." +
+                        "Guess a number between 1 and 50.");
+                    int userGuess = Int32.Parse(Console.ReadLine());
+                    Console.Clear();
+                    while (true)
                     {
-                        Console.WriteLine("please enter a number from 1 to 50");
-                        userGuess = Int32.Parse(Console.ReadLine());
-                        Console.Clear();
-                    }
-                    if (userGuess < numberToGuess)
-                    {
-                        Console.WriteLine("try higher");
-                        userGuess = Int32.Parse(Console.ReadLine());
-                        Console.Clear();
-                    }
-                    else if (userGuess > numberToGuess)
-                    {
-                        Console.WriteLine("try lower");
-                        userGuess = Int32.Parse(Console.ReadLine());
-                        Console.Clear();
-                    }
-                    else if (userGuess == numberToGuess)
-                    {
-                        Console.WriteLine("Congrats You guessed the right number");
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input");
-                        userGuess = Int32.Parse(Console.ReadLine());
+                        if (userGuess <= 0 || userGuess > 50)
+                        {
+                            Console.WriteLine("please enter a number from 1 to 50");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                            Console.Clear();
+                        }
+                        if (userGuess < numberToGuess)
+                        {
+                            Console.WriteLine("try higher");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                            Console.Clear();
+                        }
+                        else if (userGuess > numberToGuess)
+                        {
+                            Console.WriteLine("try lower");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                            Console.Clear();
+                        }
+                        else if (userGuess == numberToGuess)
+                        {
+                            Console.WriteLine("Congrats You guessed the right number");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input");
+                            userGuess = Int32.Parse(Console.ReadLine());
+                        }
                     }
                 }
-            }
-            else
-            {
-                Console.WriteLine("Invalid difficulty selection.");
+                else
+                {
+                    Console.WriteLine("Invalid difficulty selection.");
+                }
             }
         }
     }
